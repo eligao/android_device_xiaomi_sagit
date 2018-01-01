1d18ee6dfc3dad2bf9f088c5ab6d949e91db8f06
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-01 18:16:09 +0100

    sagit: camera: build Snap
    Change-Id: Icb7e7cd294c2747309dd70ab8e2c887bdb44872f

--- device.mk
+++ device.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
     libshim_MiCamera \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
