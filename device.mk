35c4b33bc4d3f7d57da8764499a90bf89cdefa4c
Bruno Martins <bgcngm@gmail.com>
2018-01-09 10:42:27 +0000

    sagit: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
     * Leave out sensors cause compass sometimes doesn't work
       after booting.
    
    Change-Id: I1e4c1efa54b2a4b4984af9ac5286936106e5e34e

--- device.mk
+++ device.mk
@@ -82,7 +82,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
@@ -144,8 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl
-
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8998 \
@@ -163,7 +165,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
@@ -213,7 +217,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service \
+    android.hardware.health@1.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -346,7 +350,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl \
     ims-ext-common \
     librmnetctl \
     libxml2 \
