1c7ae000c0c521cb5d5cd75119be5d35d8ee2e7e
blankaf <realblankaf@gmail.com>
2018-02-14 22:55:19 +0100

    sagit: Add build fingerprint
    From Global Stable V9.2.3.0.OCAMIEK (MIUI 9 Oreo)

--- lineage.mk
+++ lineage.mk
@@ -10,3 +10,9 @@ PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="sagit" \
+    PRODUCT_NAME="sagit" \
+    BUILD_FINGERPRINT="Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys" \
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
